d657f7f68496bb3fec2e3daca78e0ef95f32e9b2
leeminkan <nguyenhoangphuc.kan@gmail.com>
2024-08-24 09:59:34 +0700

    feat(hector): complete assignment 17

--- hecto/src/editor/view/line.rs
+++ hecto/src/editor/view/line.rs
@@ -1,4 +1,4 @@
-use std::ops::Range;
+use std::{fmt, ops::Range};
 use unicode_segmentation::UnicodeSegmentation;
 use unicode_width::UnicodeWidthStr;
 
@@ -87,6 +87,12 @@ impl Line {
         self.fragments = Self::str_to_fragments(&result);
     }
 
+    pub fn append(&mut self, other: &Self) {
+        let mut concat = self.to_string();
+        concat.push_str(&other.to_string());
+        self.fragments = Self::str_to_fragments(&concat);
+    }
+
     pub fn get_visible_graphemes(&self, range: Range<usize>) -> String {
         if range.start >= range.end {
             return String::new();
@@ -146,5 +152,16 @@ impl Line {
             }
             _ => None,
         }
+    }
+}
+
+impl fmt::Display for Line {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        let result: String = self
+            .fragments
+            .iter()
+            .map(|fragment| fragment.grapheme.clone())
+            .collect();
+        write!(formatter, "{result}")
     }
 }
